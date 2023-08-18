73868338ccf971646281b4b136d6aacd9bee372d
antony_dickson <antonydickson@centizen.com>
2023-08-18 09:14:11 +0530

    new project

--- src/Pages/News.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function News() {
-  return (
-    <iframe src='https://widgets.sociablekit.com/youtube-channel-videos/iframe/158679' frameborder='0' width='100%' height='1000'></iframe>
-  )
-}
--- src/Pages/programs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function programs() {
-  return (
-    <div>programs</div>
-  )
-}
